3236802fe6682caed399cbf0d7118be64add2750
utkarshgoeslike <utkarshtripathi.csai27@jecrc.ac.in>
2026-06-08 01:07:29 +0530

    Support TLS encrypted turns endpoints for stricter carrier networks

--- public/js/viewer.js
+++ public/js/viewer.js
@@ -2,12 +2,20 @@
   const socket = window.streamSocket;
   const utils = window.StreamUtils;
   const iceServers = [
-    { urls: 'stun:stun.l.google.com:19302' },
+    {
+      urls: [
+        'stun:stun.l.google.com:19302',
+        'stun:stun1.l.google.com:19302',
+        'stun:stun2.l.google.com:19302'
+      ]
+    },
     {
       urls: [
         'turn:openrelay.metered.ca:80',
         'turn:openrelay.metered.ca:443',
-        'turn:openrelay.metered.ca:443?transport=tcp'
+        'turn:openrelay.metered.ca:443?transport=tcp',
+        'turns:openrelay.metered.ca:443',
+        'turns:openrelay.metered.ca:443?transport=tcp'
       ],
       username: 'openrelayproject',
       credential: 'openrelayproject'
