d297490ed99b065228d353afda1ff602b0c3a377
utkarshgoeslike <utkarshtripathi.csai27@jecrc.ac.in>
2026-06-08 00:58:16 +0530

    Add public TURN server for cross-network WebRTC media streaming

--- public/js/viewer.js
+++ public/js/viewer.js
@@ -1,7 +1,18 @@
 (function () {
   const socket = window.streamSocket;
   const utils = window.StreamUtils;
-  const iceServers = [{ urls: 'stun:stun.l.google.com:19302' }];
+  const iceServers = [
+    { urls: 'stun:stun.l.google.com:19302' },
+    {
+      urls: [
+        'turn:openrelay.metered.ca:80',
+        'turn:openrelay.metered.ca:443',
+        'turn:openrelay.metered.ca:443?transport=tcp'
+      ],
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+    }
+  ];
 
   const remoteVideo = document.querySelector('#remoteVideo');
   const codeInput = document.querySelector('#streamCodeInput');
